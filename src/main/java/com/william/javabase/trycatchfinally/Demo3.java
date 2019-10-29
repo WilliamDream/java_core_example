9cb82aeac349c493be4795018ac4609cf49bb765
williamdream <3339642010@qq.com>
2019-10-29 13:52:12 +0800

    trycache

--- src/main/java/com/william/javabase/trycatchfinally/Demo3.java
+++ src/main/java/com/william/javabase/trycatchfinally/Demo3.java
@@ -10,15 +10,29 @@ public class Demo3 {
 	 */
 	
 	public static void main(String[] args) {
-		
-		System.out.println(getString());
-		System.out.println("---------------------");
+		System.out.println("-----------getString----------");
+//		System.out.println(getString());	//运行结果，编译期报错，缺少返回语句
+		System.out.println("------------getString1---------");
+		System.out.println(getString1());
+		System.out.println("------------getString2---------");
 		System.out.println(getString2());
 		
 	}
+	/*public static String getString() {
+		String str = "hello";
+		try {
+			int a = 1/0;
+			return str;
+		} catch (Exception e) {
+			System.out.println("执行catch");
+			str =  "catch修改为：error";
+		}finally {
+			System.out.println("执行finally");
+			str = "finally修改为：hi";
+		}
+	}*/
 	
-	
-	public static String getString() {
+	public static String getString1() {
 		String str = "hello";
 		try {
 			int a = 1/0;
@@ -27,7 +41,8 @@ public class Demo3 {
 			System.out.println("执行catch");
 			return "error";
 		}finally {
-			return str = "hi";
+			System.out.println("执行finally");
+			str = "hi";
 		}
 	}
 	
@@ -40,7 +55,8 @@ public class Demo3 {
 			System.out.println("执行catch");
 			return "error";
 		}finally {
-			str = "hi";
+			System.out.println("执行finally");
+			return str = "hi";
 		}
 	}
 	
